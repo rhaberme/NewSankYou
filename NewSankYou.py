81a29612a974d500cb924a36b41ff5b0a099fe9d
rhaberme <rhaberme@rhrk.uni-kl.de>
2022-11-25 15:24:32 +0100

    latest changes

--- NewSankYou.py
+++ NewSankYou.py
@@ -37,8 +37,7 @@ def get_tables(db_file, result_num_):
 
 
 def create_flows_dict(data_df):
-    month_key_list = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN',
-                      'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
+    month_key_list = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
     flows = {}
 
     for ind in data_df.index:
